bd24a971926f09045b75246059388bb88a247ceb
yana mulyana <yana.qhj@gmail.com>
2019-11-21 11:55:29 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (err, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
